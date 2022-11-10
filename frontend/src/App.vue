16001703661553c733fbd6889ef70388ea04f74d
Andres Mora <estfabiber2@hotmail.com>
2022-11-10 00:11:59 -0500

    Se limpia backend.

--- frontend/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container is-max-desktop">
-    <router-view />
-  </div>
-</template>
- 
-<style>
-@import "~bulma/css/bulma.css";
-</style>
--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-// import axios
-import axios from 'axios'
-// define baseURL
-axios.defaults.baseURL = 'http://localhost:8080/'
- 
-createApp(App).use(router).mount('#app')
